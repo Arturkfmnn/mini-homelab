03acf32db21de1735011d6f68968c8316964919e
Artur Kaufmann <arturkaufmann@gmail.com>
2025-10-12 09:41:25 +0000

    Munin: Add netcat

--- machines/munin/configuration.nix
+++ machines/munin/configuration.nix
@@ -88,6 +88,7 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    netcat-gnu
     wget
     git
     curl
